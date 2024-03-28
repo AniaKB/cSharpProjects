cf3a3a7a7b90b1ed68214a87a6366d7a6e0247ee
Ania <aniakbennett@gmail.com>
2024-03-28 19:10:47 -0400

    adding new way to display an instance of a class

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello C#!");
 
-Random dice = new Random();
+Random dice = new(); // same as Random dice = new Random();
 int roll = dice.Next(1, 7);
 Console.WriteLine(roll);
